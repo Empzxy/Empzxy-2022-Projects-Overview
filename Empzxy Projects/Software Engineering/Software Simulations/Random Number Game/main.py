7f5d2373d7b476c3e6016585b7f21e21070d651a
Empzxy <88317328+Empzxy@users.noreply.github.com>
2022-01-04 18:12:17 +0000

    Create main.py

---
+++ Empzxy Projects/Software Engineering/Software Simulations/Random Number Game/main.py
@@ -0,0 +1,31 @@
+import random 
+
+def Main():
+    player1 = 0
+    player1wins = 0
+    player2 = 0
+    player2wins = 0
+    rounds = 1
+    
+
+    while rounds != 10:
+        player1 = dice_roll()
+        player2 = dice_roll()
+        print("Player 1 Roll: " + str(player1))
+        print("Player 2 Roll: " + str(player2))
+
+        if player1 == player2:
+            print("Draw!")
+        elif player1 > player2:
+            player1wins = player1wins + 1
+            print("Player 1 Wins!")
+        else: 
+            player2wins = player2wins + 1
+            print("Player 2 Wins!")
+
+        rounds = rounds + 1 
+
+def dice_roll():
+    diceRoll = random.randint(1, 6)
+    return diceRoll 
+Main()
